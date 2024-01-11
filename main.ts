3162ff80a73a7c7904723ea8cdd00c58201a779b
varchodi <rauschenrobert@gmail.com>
2024-01-11 19:59:08 +0300

    params add

--- main.ts
+++ main.ts
@@ -3,18 +3,40 @@ import express from "express"
 const app = express();
 const port = process.env.PORT || 3000;
 
+const mockUsers = [
+    { id: 1, username: "anson", displayName: "Anson" },
+    { id: 2, username: "green", displayName: "Green" },
+    { id: 3, username: "Him", displayName: "Hey" },
+];
+
 //routes and rquests
 //app.get(route,handleler)
 app.get("/", (req, response) => {
     response.status(201).send({ msg: "hello world " });
 })
 
+
 app.get('/api/users', (req, response) => {
-    response.send([
-        { id: 1, username: "anson", displayName: "Anson" },
-        { id: 1, username: "green", displayName: "Green" },
-        { id: 1, username: "Him", displayName:"Hey"},
-    ])
+    response.send(mockUsers);
+})
+
+//route parameter (path/:paramName)
+app.get('/api/users/:id', (req, res) => {
+    //get params req.params
+    const { id } = req.params;
+
+    //optional, 9check if ID is int 
+    if (isNaN(parseInt(id))) {
+        return res.status(400).send({
+            msg:" bad request,ivalid ID"
+        })
+    }
+
+    //parseInt(str); covert str to int
+    const findUser = mockUsers.find((user) => user.id === parseInt(id))
+    if(!findUser) return res.status(400).send({msg:`no user with id:${id}`})
+    res.send(findUser);
+
 })
 
 app.get('/api/products', (req, res) => {
