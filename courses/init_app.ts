ab4d07209ea1bd7b589c63c5d418fba19c47759d
varchodi <rauschenrobert@gmail.com>
2024-01-11 19:22:46 +0300

    app initialisation

--- courses/init_app.ts
+++ courses/init_app.ts
@@ -1,8 +1,12 @@
+// here how to init express app
+
 import express from "express"
 
+//create app
 const app = express();
 const port = process.env.PORT || 3000;
 
+//run server ...
 app.listen(port, () => {
     console.log(`running on podt ${port}`)
 })
